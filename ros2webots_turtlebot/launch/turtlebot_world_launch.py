d495f4865693351a322bf03ac338ae8bed6548d6
Mohammad Javad Zallaghi <mohammadjavadzallaghi@gmail.com>
2023-12-26 08:53:11 +0330

    launch file of turtle_bot has been updated + how set share dir

--- ros2webots_turtlebot/launch/turtlebot_world_launch.py
+++ ros2webots_turtlebot/launch/turtlebot_world_launch.py
@@ -2,18 +2,39 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.substitutions.path_join_substitution import PathJoinSubstitution
 from webots_ros2_driver.webots_launcher import WebotsLauncher
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+import launch
 
 def generate_launch_description():
 
-    # start webots 
-    # webots = WebotsLauncher (
+    package_dir = get_package_share_directory('ros2webots_turtlebot')
+    world = LaunchConfiguration('world')
 
-    # )
+    # start webots 
+    webots = WebotsLauncher (
+        world=PathJoinSubstitution([package_dir, 'worlds', world]),
+        ros2_supervisor=True
+    )
+
     return LaunchDescription([
-        Node(
-            package='webots_ros2_dev_1',
-            namespace='sim_node_run_1',
-            executable='sim_node_ros2webots',
-            name='sim'
+        DeclareLaunchArgument(
+            'world',
+            default_value='world_roboticknowledge.wbt',
+            description='Choose one of the world files from `/webots_ros2_epuck/world` directory'
         ),
+        webots,
+        webots._supervisor,
+
+
+        # This action will kill all nodes once the Webots simulation has exited
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=webots,
+                on_exit=[
+                    launch.actions.EmitEvent(event=launch.events.Shutdown())
+                ],
+            )
+        )
     ])
--- ros2webots_turtlebot/setup.py
+++ ros2webots_turtlebot/setup.py
@@ -5,18 +5,32 @@ from glob import glob
 from setuptools import find_packages, setup
 
 package_name = 'ros2webots_turtlebot'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', [
+    'resource/' + package_name
+]))
+data_files.append(('share/' + package_name + '/launch', [
+    'launch/test_launch.py',
+    'launch/turtlebot_world_launch.py',
+]))
+data_files.append(('share/' + package_name + '/worlds', [
+    'worlds/world_roboticknowledge.wbt',
+]))
+# data_files.append(('share/' + package_name + '/protos', [
+#     'protos/LegoTallInterval.proto',
+# ]))
+# data_files.append(('share/' + package_name + '/resource', [
+#     'resource/all.rviz',
+# ]))
+data_files.append(('share/' + package_name, [
+    'package.xml'
+]))
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        # Include all launch files.
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='mjavadzallaghi',
