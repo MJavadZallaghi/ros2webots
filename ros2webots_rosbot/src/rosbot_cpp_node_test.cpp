ce3e8809e79728f38ea1faa4e6d93afdec1b55e3
Mohammad Javad Zallaghi <mohammadjavadzallaghi@gmail.com>
2023-12-30 13:30:51 +0330

    rosbot package has been added with cmake approach

---
+++ ros2webots_rosbot/src/rosbot_cpp_node_test.cpp
@@ -0,0 +1,54 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "ros2webots_rosbot/rosbot_cpp_node_test.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+using namespace std::chrono_literals;
+
+class MinimalPublisher : public rclcpp::Node
+{
+  public:
+    MinimalPublisher()
+    : Node("minimal_publisher"), count_(0)
+    {
+      publisher_ = this->create_publisher<std_msgs::msg::String>("topic_test", 10);
+      timer_ = this->create_wall_timer(
+      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+
+      // Adding the subscriber to the "topic_test"
+      subscriber_ = this->create_subscription<std_msgs::msg::String>(
+        "topic_test", 10, std::bind(&MinimalPublisher::subscriber_callback, this, std::placeholders::_1));
+    }
+
+  private:
+    void timer_callback()
+    {
+      auto message = std_msgs::msg::String();
+      message.data = "Hello, world! " + std::to_string(count_++);
+      RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+      publisher_->publish(message);
+    }
+
+    void subscriber_callback(const std_msgs::msg::String::SharedPtr msg)
+    {
+      RCLCPP_INFO(this->get_logger(), "Received: '%s'", msg->data.c_str());
+    }
+
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscriber_;
+    size_t count_;
+};
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
