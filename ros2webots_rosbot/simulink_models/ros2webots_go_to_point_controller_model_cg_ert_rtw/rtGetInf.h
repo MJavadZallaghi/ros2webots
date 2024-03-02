53f254b6e17ed883ea1881f1aa8342938c1102f0
Mohammad Javad Zallaghi <mohammadjavadzallaghi@gmail.com>
2024-03-02 17:12:12 +0330

    Go-to-point controller has been develope and code generated for it

---
+++ ros2webots_rosbot/simulink_models/ros2webots_go_to_point_controller_model_cg_ert_rtw/rtGetInf.h
@@ -0,0 +1,49 @@
+//
+// File: rtGetInf.h
+//
+// Code generated for Simulink model 'ros2webots_go_to_point_controller_model_cg'.
+//
+// Model version                  : 1.347
+// Simulink Coder version         : 9.9 (R2023a) 19-Nov-2022
+// C/C++ source code generated on : Sat Mar  2 16:57:53 2024
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Intel->x86-64 (Windows64)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_rtGetInf_h_
+#define RTW_HEADER_rtGetInf_h_
+
+extern "C"
+{
+
+#include "rt_nonfinite.h"
+
+}
+
+#include "rtwtypes.h"
+#ifdef __cplusplus
+
+extern "C"
+{
+
+#endif
+
+  extern real_T rtGetInf(void);
+  extern real32_T rtGetInfF(void);
+  extern real_T rtGetMinusInf(void);
+  extern real32_T rtGetMinusInfF(void);
+
+#ifdef __cplusplus
+
+}                                      // extern "C"
+
+#endif
+#endif                                 // RTW_HEADER_rtGetInf_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
